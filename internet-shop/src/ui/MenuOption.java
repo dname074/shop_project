19ada8cdb0d10e40837644b48f06b144da9f450a
Damian Holona <damianholona074@gmail.com>
2025-11-21 21:17:32 +0100

    + create UserInterface class and enum for menu options

---
+++ internet-shop/src/ui/MenuOption.java
@@ -0,0 +1,25 @@
+package ui;
+
+enum MenuOption {
+    SHOW_PRODUCTS(1, "Przeglądaj produkty"),
+    ADD_TO_CART(2, "Dodaj do koszyka"),
+    REMOVE_FROM_CART(3, "Usuń z koszyka"),
+    PLACE_AN_ORDER(4, "Złóż zamówienie");
+
+    private final int value;
+    private final String description;
+
+    MenuOption(int value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    public static MenuOption getOptionFromInt(int number) {
+        return MenuOption.values()[number-1];
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d. %s", value, description);
+    }
+}
---
+++ internet-shop/src/ui/UserInteface.java
@@ -0,0 +1,13 @@
+package ui;
+
+public class UserInteface {
+    public void start() {
+
+    }
+
+    private void print() {
+        for (MenuOption option : MenuOption.values()) {
+
+        }
+    }
+}
