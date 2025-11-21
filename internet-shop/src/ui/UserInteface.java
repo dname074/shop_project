bf04c4ffe52c42ce91e06a5621dfc96218f373ff
Damian Holona <damianholona074@gmail.com>
2025-11-21 21:27:04 +0100

    + print menu in UserInterface class

--- internet-shop/src/ui/UserInteface.java
+++ internet-shop/src/ui/UserInteface.java
@@ -1,13 +1,19 @@
 package ui;
 
+import io.DataPrinter;
+
 public class UserInteface {
     public void start() {
+        boolean running = true;
 
+        while (running) {
+            printMenu();
+        }
     }
 
-    private void print() {
+    private void printMenu() {
         for (MenuOption option : MenuOption.values()) {
-
+            DataPrinter.print(option.toString());
         }
     }
 }
